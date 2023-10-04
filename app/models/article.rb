53c94a83efed72314024d86dd1185490735f9ab6
Semyon Turitsyn <s.turitsyn@quickinim.com>
2023-10-04 22:58:09 +0400

    deploy

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
   validates :header, :body, presence: true
-  has_many :comments
-  has_many :reaction
+  has_many :comments, dependent: :destroy
+  has_many :reaction, dependent: :destroy
 
   scope :pity, -> { where("articles.created_at < ?", Time.now - 10.seconds).includes(:reaction).where(reactions: {id: nil})}
 
